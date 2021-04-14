ba18a5700e398c157578c637beaebffa80873ac2
sobamchan <oh.sore.sore.soutarou@gmail.com>
2021-04-14 21:27:28 +0900

    added zsh alias to create yesterday entry file.

--- .zsh/rc/alias.zsh
+++ .zsh/rc/alias.zsh
@@ -2,4 +2,5 @@ alias la="ls -a"
 alias ll="ls -al" 
 alias up="cd .."
 alias d='nvim /Users/sobamchan/PROJECT/WEB/sobamchan.github.io/_journals/$(date "+%Y-%m-%d-journal.md")'
+alias yd='nvim /Users/sobamchan/PROJECT/WEB/sobamchan.github.io/_journals/$(date -v -1d "+%Y-%m-%d-journal.md")'
 alias note="nvim /Users/sobamchan/PROJECT/WEB/sobamchan.github.io/_notes/$(date "+%Y-%m-01-note.md")"
